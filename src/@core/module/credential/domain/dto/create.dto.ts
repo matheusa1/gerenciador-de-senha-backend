381537f8ad9b2724a118f380e581ce403381e885
Matheus Andrade <2002matheus@gmail.com>
2025-08-22 06:53:52 -0300

    feat: ajustes nos campos opcionais relacionados à entidade de credenciais
    ### Descrição:
    - Tornados opcionais os campos `password`, `username`, `url` e `categoryId` no DTO de criação de credenciais com o uso de `@IsOptional`;
    - Atualizado o esquema Prisma para refletir os campos opcionais no modelo da tabela `credential`.

--- src/@core/module/credential/domain/dto/create.dto.ts
+++ src/@core/module/credential/domain/dto/create.dto.ts
@@ -12,16 +12,16 @@ export class CreateCredentialDto {
   name: string;
 
   @IsString({ message: 'A senha deve ser uma string.' })
-  @IsNotEmpty({ message: 'A senha é obrigatória.' })
-  password: string;
+  @IsOptional()
+  password?: string;
 
   @IsString({ message: 'O nome de usuário deve ser uma string.' })
-  @IsNotEmpty({ message: 'O nome de usuário é obrigatório.' })
-  username: string;
+  @IsOptional()
+  username?: string;
 
   @IsUrl(undefined, { message: 'A URL deve ser um endereço válido.' })
-  @IsNotEmpty({ message: 'A URL é obrigatória.' })
-  url: string;
+  @IsOptional()
+  url?: string;
 
   @IsString({ message: 'As notas devem ser uma string.' })
   @IsOptional()
@@ -29,5 +29,5 @@ export class CreateCredentialDto {
 
   @IsUUID(7, { message: 'O ID da categoria deve ser um UUID válido.' })
   @IsOptional()
-  categoryId: string;
+  categoryId?: string;
 }

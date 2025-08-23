a7c47b368287025f28f5b8c47b309c274701d37c
Matheus Andrade <2002matheus@gmail.com>
2025-08-23 18:12:37 -0300

    feat: adicionado transformações para campos opcionais no DTO de criação de credenciais
    ### Descrição:
    - Adicionado o decorator `@Transform` para os campos `password`, `username`, `url`, `notes` e `categoryId` no `CreateCredentialDto`;
    - Transformação substitui valores vazios (`''`) por `undefined`, garantindo consistência na validação de campos opcionais.

--- src/@core/module/credential/domain/dto/create.dto.ts
+++ src/@core/module/credential/domain/dto/create.dto.ts
@@ -5,28 +5,34 @@ import {
   IsUrl,
   IsUUID,
 } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateCredentialDto {
   @IsString({ message: 'O nome deve ser uma string.' })
   @IsNotEmpty({ message: 'O nome é obrigatório' })
   name: string;
 
+  @Transform(({ value }) => (value === '' ? undefined : value))
   @IsString({ message: 'A senha deve ser uma string.' })
   @IsOptional()
   password?: string;
 
+  @Transform(({ value }) => (value === '' ? undefined : value))
   @IsString({ message: 'O nome de usuário deve ser uma string.' })
   @IsOptional()
   username?: string;
 
+  @Transform(({ value }) => (value === '' ? undefined : value))
   @IsUrl(undefined, { message: 'A URL deve ser um endereço válido.' })
   @IsOptional()
   url?: string;
 
+  @Transform(({ value }) => (value === '' ? undefined : value))
   @IsString({ message: 'As notas devem ser uma string.' })
   @IsOptional()
   notes?: string;
 
+  @Transform(({ value }) => (value === '' ? undefined : value))
   @IsUUID(7, { message: 'O ID da categoria deve ser um UUID válido.' })
   @IsOptional()
   categoryId?: string;
